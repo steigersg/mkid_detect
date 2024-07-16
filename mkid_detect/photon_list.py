ecbf9d6ff50a8c5375b2393612526f451b8a5f0a
Sarah Steiger <ssteiger@stsci.edu>
2024-07-16 13:40:58 -0400

    create photon_list.py to handle photon list generation and manipulation

---
+++ mkid_detect/photon_list.py
@@ -0,0 +1,34 @@
+from tables import *
+
+
+class Photon(IsDescription):
+    x = UInt16Col()
+    y = UInt16Col()
+    wavelength = Float64Col()
+    time = Float64Col()
+
+
+class PhotonList:
+    def __init__(self, start):
+        self.start = start
+        self.name = f"{self.start}.h5"
+
+        self.table = None
+        self.h5file = open_file(self.name, mode="w", title="Photon Table")
+
+        group = self.h5file.create_group("/", 'MKID', 'MKID Information')
+
+        self.table = self.h5file.create_table(group, 'readout', Photon, "MKID Readout File")
+
+    def add_photon(self, x, y, wavelength, time):
+        photon = self.table.row
+        photon["x"] = x
+        photon["y"] = y
+        photon["wavelength"] = wavelength
+        photon["time"] = time
+        photon.append()
+        self.table.flush()
+
+    def get_column(self, col_name):
+        table = self.h5file.root.MKID.readout
+        return [x[col_name] for x in table.iterrows()]
