badda41b884bc4815bf997a84764685a800546b0
Sarah Steiger <ssteiger@stsci.edu>
2024-07-18 13:58:02 -0400

    add query function to PhotonList

--- mkid_detect/photon_list.py
+++ mkid_detect/photon_list.py
@@ -26,3 +26,31 @@ class PhotonList:
     def get_column(self, col_name):
         table = self.h5file.root.MKID.readout
         return [x[col_name] for x in table.iterrows()]
+
+    def query_photons(self, start_wvl=None, stop_wvl=None, start_time=None, stop_time=None):
+        if start_wvl is None and stop_wvl is None:
+            wvl_condition = ''
+        elif start_wvl and stop_wvl:
+            wvl_condition = f'({start_wvl} < wavelength) & (wavelength < {stop_wvl})'
+        elif start_wvl and not stop_wvl:
+            wvl_condition = f'({start_wvl} < wavelength)'
+        else:
+            wvl_condition = f'(wavelength < {stop_wvl})'
+
+        if start_time is None and stop_time is None:
+            time_condition = ''
+        elif start_time and stop_time:
+            time_condition = f'({start_time*1e6} < time) & (time < {stop_time*1e6})'
+        elif start_time and not stop_time:
+            time_condition = f'({start_time*1e6} < time)'
+        else:
+            time_condition = f'(time < {stop_time*1e6})'
+
+        if wvl_condition == '':
+            condition = time_condition
+        elif time_condition == '':
+            condition = wvl_condition
+        else:
+            condition = wvl_condition + '&' + time_condition
+
+        return self.table.read_where(condition)
