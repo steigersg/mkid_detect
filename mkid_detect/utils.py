b39a10913c0b1b4916456284b18fadf7e422312f
Sarah Steiger <ssteiger@stsci.edu>
2024-07-18 13:59:05 -0400

    make sure remove_deadtime utility input is not empty and contains ints

--- mkid_detect/utils.py
+++ mkid_detect/utils.py
@@ -3,11 +3,15 @@ import numpy as np
 def remove_deadtime(t_list, dead_time=10):
     # TODO rewrite in C to speed up
     keep_times = []
+    if len(t_list) == 0:
+        return t_list
 
     for i, t in enumerate(t_list[:-1]):
-        if t_list[i+1] - t > dead_time:
+        if (t_list[i+1] - t) > dead_time:
             keep_times.append(int(t))
         else:
             pass
 
-    return keep_times
+    keep_times.append(int(t_list[-1]))
+
+    return np.array(keep_times, dtype=int)
