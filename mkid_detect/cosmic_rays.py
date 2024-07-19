2a80837514d8bceb2b9f689cd8f015946e18ba0d
Sarah Steiger <ssteiger@stsci.edu>
2024-07-19 15:49:06 -0400

    cleaning

--- mkid_detect/cosmic_rays.py
+++ mkid_detect/cosmic_rays.py
@@ -33,31 +33,30 @@ def cosmic_rays(xdim, ydim, cr_rate, exp_time, pixel_pitch):
     cr_times: np.ndarray
         Array of times when a cosmic ray photon was registered.
     """
-    # to start just assume all pixels get saturated
     array_area = (xdim * pixel_pitch) * (ydim * pixel_pitch)
     hits_per_sec = array_area * cr_rate
     total_hits = int(hits_per_sec * exp_time)
 
-    # each hit happens ar a random time on a random pixel
+    # Each hit happens at a random time in a random pixel order.
     possible_times = np.arange(0, exp_time * 1e6, 10, dtype=int)
 
     hit_times = np.random.choice(possible_times, int(total_hits))
 
-    # use meshgrid to get every coordinate
     grid = np.meshgrid(np.arange(0, xdim, 1), np.arange(0, ydim, 1))
     cr_xs = grid[0].flatten()
     cr_ys = grid[1].flatten()
 
-    # give every pixel 1 count within +/- 10 microseconds of the hit time
-    # later can add fireball to get better timing
+    # Give every pixel 1 count within +/- 10 microseconds of the hit time.
     cr_times = []
     for i, time in enumerate(hit_times):
         cr_times.append(np.random.choice(np.arange(time-10, time+10, 1), len(cr_xs)))
 
     cr_times = np.array(cr_times, dtype=int)
-    # generate a random wavelength
+
+    # Generate a random wavelength for each cosmic ray photon.
     cr_wvls = np.random.choice(np.arange(100, 1000, 1), size=(total_hits, len(cr_xs)))
 
     cr_xs = np.tile(cr_xs, (total_hits, 1))
     cr_ys = np.tile(cr_ys, (total_hits, 1))
+
     return cr_xs, cr_ys, cr_wvls, cr_times
